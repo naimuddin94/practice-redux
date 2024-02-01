8b17f4b8fd728f8e565ed9b6c0f4a006fe7a957c
naimuddin94 <developernaimuddin@gmail.com>
2024-02-02 00:30:40 +0600

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+
+
+const Navbar = () => {
+    return (
+        <nav>
+            <h2>Logo</h2>
+            <ul className="nav-items">
+                <li>Home</li>
+                <li>Users</li>
+                <li>Contact</li>
+                <li>Login</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
